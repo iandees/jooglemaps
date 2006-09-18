f2d5706a4dd9100f74a8747f68ab665fa0157cba
ian.dees <ian.dees@18bd37d4-9619-0410-9656-fb9999c766d4>
2006-09-18 04:23:14 +0000

    Line weights.

--- trunk/NetDraw/src/com/mapki/netdraw/gui/DrawingGui.java
+++ trunk/NetDraw/src/com/mapki/netdraw/gui/DrawingGui.java
@@ -5,13 +5,10 @@ package com.mapki.netdraw.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
-import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
-import java.awt.event.WindowStateListener;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
--- trunk/NetDraw/src/com/mapki/netdraw/network/DrawersPool.java
+++ trunk/NetDraw/src/com/mapki/netdraw/network/DrawersPool.java
@@ -3,7 +3,6 @@ package com.mapki.netdraw.network;
 import java.util.Vector;
 
 import com.mapki.netdraw.Drawer;
-import com.mapki.netdraw.gui.NetDrawable;
 
 public class DrawersPool {
     private Vector<Drawer> pool;
